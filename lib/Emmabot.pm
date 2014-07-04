6e2a7352679b96a7926fa40113bbe22721ed4bd3
Carl Masak <cmasak@gmail.com>
2014-07-04 20:05:20 +0200

    pass logging test

--- lib/Emmabot.pm
+++ lib/Emmabot.pm
@@ -5,13 +5,16 @@ class Emmabot;
 
 has $.modules;
 has $.channel;
+has $.log = class { method log($, $, *@) {} };
 
 method do_daily_report {
     if $.modules.new_failures().list -> @modules {
         $.channel.report(Emmabot::FailureReport.new(:@modules, :type<new>));
+        $.log.log(0, 0);
     }
 
     if $.modules.ongoing_failures().list -> @modules {
         $.channel.report(Emmabot::FailureReport.new(:@modules, :type<ongoing>));
+        $.log.log(0, 0);
     }
 }
