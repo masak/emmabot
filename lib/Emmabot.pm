ffc83ef2b482df9fb3ded30b560862e017df4aad
Carl Masak <cmasak@gmail.com>
2014-07-04 18:31:59 +0200

    refactor: rename loop variable

--- lib/Emmabot.pm
+++ lib/Emmabot.pm
@@ -8,7 +8,7 @@ has $.modules;
 has $.channel;
 
 method do_daily_report {
-    for $.modules.new_failures() -> $failure {
-        $.channel.report(Emmabot::FailureReport.new(module => $failure));
+    for $.modules.new_failures() -> $module {
+        $.channel.report(Emmabot::FailureReport.new(:$module));
     }
 }
